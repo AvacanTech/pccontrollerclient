67b19fc1fd185ad1662f550058c9d58c6bb74eff
absofresho <contact@jeremiah.business>
2023-12-11 22:48:28 -0500

    reconnect if down

--- lib/pccontrollerclient.dart
+++ lib/pccontrollerclient.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:process_run/shell.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
@@ -7,25 +8,46 @@ void main() async {
   // Obtain the first IPv4 address
   String ipAddress = await getFirstIPv4Address();
 
-  // Set up WebSocket handler
+  // Start the server
+  startWebSocketServer(ipAddress, 4040);
+}
+
+Future<void> startWebSocketServer(String ipAddress, int port) async {
   var handler = webSocketHandler((webSocket) {
-    webSocket.stream.listen((message) async {
-      try {
-        String response = await executeCommand(message);
-        webSocket.sink.add(response);
-      } catch (e) {
-        webSocket.sink.add('Error: $e');
-      }
-    });
+    webSocket.stream.listen(
+      (message) async {
+        try {
+          String response = await executeCommand(message);
+          webSocket.sink.add(response);
+        } catch (e) {
+          webSocket.sink.add('Error: $e');
+        }
+      },
+      onDone: () {
+        // Handle disconnection
+        print('WebSocket disconnected. Attempting to reconnect...');
+        reconnect(ipAddress, port);
+      },
+      onError: (error) {
+        print('Error: $error');
+        reconnect(ipAddress, port);
+      },
+    );
   });
 
-  // Start the server
-  shelf_io.serve(handler, ipAddress, 4040).then((server) {
+  shelf_io.serve(handler, ipAddress, port).then((server) {
     print('Serving at ws://${server.address.host}:${server.port}');
   });
 }
 
-// Function to get the first IPv4 address
+void reconnect(String ipAddress, int port) {
+  // Wait for a few seconds before attempting to reconnect
+  Future.delayed(Duration(seconds: 5), () {
+    print('Attempting to restart WebSocket server...');
+    startWebSocketServer(ipAddress, port);
+  });
+}
+
 Future<String> getFirstIPv4Address() async {
   for (var interface in await NetworkInterface.list()) {
     for (var addr in interface.addresses) {
@@ -37,7 +59,6 @@ Future<String> getFirstIPv4Address() async {
   throw Exception('No IPv4 address found');
 }
 
-// Function to execute a shell command and return its output
 Future<String> executeCommand(String command) async {
   var shell = Shell();
   List<ProcessResult> result = await shell.run(command);
